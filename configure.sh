7f966b9431a3dfc99e400b0e17dc9742098fae2a
Milan Miladinovic <milan.miladinovic@mail.utoronto.ca>
2019-05-23 13:00:05 -0400

    Updated configure script

--- configure.sh
+++ configure.sh
@@ -3,14 +3,22 @@
 # todo: rename dynamic libs to force gcc to use static ones
 
 # set up, compile, and install libbtc
+echo "Installing libbtc..."
 cd libbtc
 ./autogen.sh
 ./configure --disable-wallet --disable-tools
 make check
 make install
 
+# compile and install secp256k1
+echo "Installing secp256k1..."
+cd src/secp256k1
+make
+make install
+
 # compile libbloom
-cd ../libbloom
+echo "Installing libbloom..."
+cd ../../../libbloom
 make
 
 #   *note* since all these keys are really just integers, see if it's worth it to
@@ -18,5 +26,13 @@ make
 #   performing a lot of DB reads, it might make look up faster.
 
 # create sqlite3 database with keys table
+echo "Creating Observer database..."
 cd ../db
-sqlite3 observer.db < configure.sql
+db=observer.db
+
+if [ -f "$db" ]
+then
+    echo "$db already exists."
+else
+    sqlite3 `$db` < configure.sql
+fi
