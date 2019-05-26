70981461815d852c452cba0785014fac6e88e465
Milan Miladinovic <milan.miladinovic@mail.utoronto.ca>
2019-05-26 14:34:08 -0400

    Large commit, see details:
    Big changes
    	- first update db, then check for records, then reupdate if needed.
    	- use new functions (prepare_query, push_Difference, etc) to avoid code reuse.
    
    Smaller changes
    	- changed size of query lengths
    	- changed int i to int j to keep i variable from higher scope.
    	- moved callback to key_funcs.c
    	- added generated variable

--- src/generate_keys.c
+++ src/generate_keys.c
@@ -22,7 +22,6 @@
 
 #include "keys.h"
 
-static int callback(void *NotUsed, int argc, char **argv, char **azColName);
 
 int main(int argc, char **argv) {
 
@@ -32,21 +31,22 @@ int main(int argc, char **argv) {
     }
 
 
-    clock_t start, end; // times the execution (for fun)
+    clock_t start, end; // times the execution
     start = clock();
     btc_ecc_start();
 
     const btc_chainparams* chain = &btc_chainparams_main; // mainnet
     const long count = strtol(argv[1], NULL, 10); // # of seeds to use
+    const unsigned long generated = count * PRIVATE_KEY_TYPES;
 
-    // array of keys to add to DB, default size is 20% of count
+    // array of keys to add to DB, default size is 20% of generated priv keys
     struct Array update;
-    init_Array(&update, ceil(2 * count * 0.2));
+    init_Array(&update, ceil(generated * 0.2));
 
-    // array of keys that may or may not be in DB, must check.
-    // default size is 1% of count, since bloom filter has error rate of 1%
+    // array of keys that may or may not be in DB, must check. Default size is
+    // 1% of generated priv keys, since the bloom filter has error rate of 1%
     struct Array check;
-    init_Array(&check, ceil(2 * count * 0.01));
+    init_Array(&check, ceil(generated * 0.01));
 
     /** Private Key Bloom Filter
      * 
@@ -107,8 +107,8 @@ int main(int argc, char **argv) {
         keys[1] = back_pad;
 
         // add private keys to bloom filter
-        for (int i = 0; i < PRIVATE_KEY_TYPES; i++) {
-            int exists = bloom_add(&priv_bloom, keys[i], MAX_BUF - 1);
+        for (int j = 0; j < PRIVATE_KEY_TYPES; j++) {
+            int exists = bloom_add(&priv_bloom, keys[j], MAX_BUF - 1);
             if (exists < 0) {
                 fprintf(stderr, "Bloom filter not initialized\n");
                 exit(1);
@@ -125,7 +125,7 @@ int main(int argc, char **argv) {
 
             btc_privkey_init(&key);
             btc_pubkey_init(&pubkey);
-            create_pubkey(keys[i], &key, &pubkey); // fills priv & pub keys
+            create_pubkey(keys[j], &key, &pubkey); // fills priv & pub keys
 
             btc_pubkey_getaddr_p2pkh(&pubkey, chain, address_p2pkh);
             btc_pubkey_getaddr_p2sh_p2wpkh(&pubkey, chain, address_p2sh_p2wpkh);
@@ -139,14 +139,14 @@ int main(int argc, char **argv) {
                 perror("malloc");
                 exit(1);
             }
-            fill_key_set(set, keys[i], seed, address_p2pkh,
+            fill_key_set(set, keys[j], seed, address_p2pkh,
                             address_p2sh_p2wpkh, address_p2wpkh);
 
             #ifdef DEBUG
                 char pubkey_hex[sizeout];
                 btc_pubkey_get_hex(&pubkey, pubkey_hex, &sizeout);
 
-                printf("\nPrivate key: %s\n", keys[i]);
+                printf("\nPrivate key: %s\n", keys[j]);
                 printf("Public Key: %s\n", pubkey_hex);
                 printf("P2PKH: %s\n", address_p2pkh);
                 printf("P2SH: %s\n", address_p2sh_p2wpkh);
@@ -169,90 +169,95 @@ int main(int argc, char **argv) {
     }
     end = clock();
 
-    printf("\nTook %f seconds to generate %ld keys\n", ((double) end - start)/CLOCKS_PER_SEC, count * 2);
+    printf("\nTook %f seconds to generate %ld key sets.\n",
+           ((double) end - start)/CLOCKS_PER_SEC, generated);
 
     fclose(fname);
     bloom_save(&priv_bloom, "private_key_filter.b");
-    printf("False Positives: %d\nFalse Positive Rate: %f\n", false_positive_count, ((double) false_positive_count / (2 * count)));
+    printf("Bloom filter caught %d records.\n", false_positive_count);
 
     // create the sql queries
-    // TODO: do the check query first so we can add them to update list before
-    int query_len = 240; // assume one statement is ~240 bytes (can realloc)
-
-    // size of a query * number of queries needed
-    char *update_sql_query = malloc(sizeof(char) * query_len * update.used);
-    if (update_sql_query == NULL) {
-        perror("malloc");
-        exit(1);
-    }
-
-    clock_t build_start = clock();
-
-    if (build_update_query(&update, &update_sql_query, query_len) == 1) {
-        exit(1);
-    }
-
-    end = clock();
-
-    printf("Took %f seconds to build the query!\n", ((double) end - build_start)/CLOCKS_PER_SEC);
-
+    int check_len = 75; // check statement ~75 bytes
+    int update_len = 240; // update statement ~240 bytes
 
     sqlite3 *db;
     char *zErrMsg = 0;
     int rc;
-    clock_t db_write_start = clock();
+
     rc = sqlite3_open("../db/Observer.db", &db);
     if (rc) {
         fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
         exit(1);
     }
 
-    rc = sqlite3_exec(db, update_sql_query, callback, 0, &zErrMsg);
-    free(update_sql_query);
+    // update database
+    char *update_sql_query;
+    if (update.used > 0) {
+        if (prepare_query(&update, &update_sql_query, update_len, UPDATE) == 1){
+            fprintf(stderr, "Failed to build query\n");
+            exit(1);
+        }
 
-    if (rc != SQLITE_OK ) {
-        fprintf(stderr, "SQL error: %s\n", zErrMsg);
-        sqlite3_free(zErrMsg);
+        rc = sqlite3_exec(db, update_sql_query, callback, 0, &zErrMsg);
+        free(update_sql_query);
+
+        if (rc != SQLITE_OK ) {
+            fprintf(stderr, "SQL error: %s\n", zErrMsg);
+            sqlite3_free(zErrMsg);
+        } else {
+            printf("Wrote %zu records to the keys table.\n", update.used);
+        }
     }
 
+    free_Array(&update); // no longer need these records
+    // reallocate because we might fill with records that failed bloom filter
+    init_Array(&update, ceil(0.5 * check.used));
 
-    // TODO put the check query first so we can write everything once.
-    // char *check_sql_query = malloc(sizeof(char) * query_len); // 500 bytes to start
-    // // check query
-    // for (int i = 0; i < check.used; i++) {
-    //     char *values = sqlite3_mprintf("SELECT privkey FROM keys WHERE "\
-    //                                    "privkey='%q'; ",
-    //                                    check.array[i]->private);
-    //     if (values == NULL) {
-    //         fprintf(stderr, "Could not allocate memory for insert query.");
-    //         exit(1);
-    //     }
-    //     // check if we need to reallocate the query
-    //     if (strlen(values) + strlen(check_sql_query) >= query_len - 1) {
-    //         check_sql_query = realloc(check_sql_query,
-    //                                    2 * query_len * sizeof(char));
-    //         query_len *= 2;
-    //     }
-    //     strcat(check_sql_query, values);
-    //     sqlite3_free(values);
-    // }
-    sqlite3_close(db);
+    // check database for records
+    char *check_sql_query;
+    if (check.used > 0) {
+        if (prepare_query(&check, &check_sql_query, check_len, CHECK) == 1) {
+            fprintf(stderr, "Failed to build query\n");
+            exit(1);
+        }
 
-    end = clock();
-    printf("Took %f seconds to write the query!\n", ((double) db_write_start - start)/CLOCKS_PER_SEC);
+        struct Array exists; // array of elements that were found in the db
+        init_Array(&exists, ceil(2 * check.used * 0.5)); // TODO: determine best size
 
-    free_Array(&update);
+        rc = sqlite3_exec(db, check_sql_query, callback, &exists, &zErrMsg);
+        if (rc != SQLITE_OK ) {
+            fprintf(stderr, "SQL error: %s\n", zErrMsg);
+            sqlite3_free(zErrMsg);
+        }
+        printf("%zu of the %zu records generated were already stored in the "\
+               "database.\n", exists.used, generated);
+        free(check_sql_query);
+        push_Difference(&exists, &check, &update);
+        free_Array(&exists);
+    }
     free_Array(&check);
 
-    return 0;
-}
+    // add records that had to be checked (if there are any)
+    if (update.used > 0) {
+        if (prepare_query(&update, &update_sql_query, update_len, UPDATE) == 1){
+            fprintf(stderr, "Failed to build query\n");
+            exit(1);
+        }
 
-// TODO rewrite CALLBACK function for our purposes
-static int callback(void *NotUsed, int argc, char **argv, char **azColName) {
-   int i;
-   for(i = 0; i<argc; i++) {
-      printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
-   }
-   printf("\n");
-   return 0;
+        rc = sqlite3_exec(db, update_sql_query, callback, 0, &zErrMsg);
+        free(update_sql_query);
+
+        if (rc != SQLITE_OK ) {
+            fprintf(stderr, "SQL error: %s\n", zErrMsg);
+            sqlite3_free(zErrMsg);
+        } else {
+            printf("Wrote an additional %zu records to the keys table.\n",
+                   update.used);
+        }
+    }
+
+    free_Array(&update);
+    sqlite3_close(db);
+
+    return 0;
 }
