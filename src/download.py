4203fb84f44e8259ae830e244e4fa7218ec17c5c
Milan Miladinovic <milan.miladinovic@mail.utoronto.ca>
2019-06-19 00:52:36 -0400

    SCAN-1 Made list a set. Added DB connection.

--- src/download.py
+++ src/download.py
@@ -12,26 +12,42 @@ import sqlite3
 
 # current block height
 block = 0
-addresses = []
+# use a set because we don't want to waste time sorting and making a list unique
+addresses = set()
 
 # make first request, then loop and check response
 url = "https://blockchain.info/block-height/{}?format=json".format(block)
 response = requests.get(url)
 
 while (response.json()['blocks'][0]['next_block'] != []):
-# for i in range(3):
-    # add all output addresses in this block to our used addresses list
+# for i in range(40): # uncomment for a test loop
+    # add all output addresses in this block to our used addresses set
     # get the next block
-    print("Searching block: {}".format(block))
     block += 1
     for i in response.json()['blocks'][0]['tx']:
         for j in i['out']:
             if 'addr' in j:
-                addresses.append(j['addr'])
+                # we could check if in set then try to add to db here
+                addresses.add(j['addr'])
     url = "https://blockchain.info/block-height/{}?format=json".format(block)
     response = requests.get(url)
 
-# make sure addresses are unique (they definitiely won't be by default)
-# build insert queries
-for i in addresses:
-    print(i)
+# add to database
+db = "../db/observer.db"
+try:
+    conn = sqlite3.connect(db)
+    cur = conn.cursor()
+
+    # build query
+    start = "BEGIN;"
+    cur.execute(start)
+    for i in addresses:
+        sql = "INSERT INTO usedAddresses VALUES ('{}');".format(i)
+        cur.execute(sql)
+
+    end = "COMMIT;"
+    cur.execute(end)
+    conn.close()
+
+except ValueError as e:
+    print(e)
