ec807c114ce4dd7db4dae8994100469ee0247e03
MellowYarker <milanmiladinovic@outlook.com>
2019-07-11 20:38:36 -0400

    OBS-2 Make db writeable & make configure understand user input.

--- configure.sh
+++ configure.sh
@@ -20,7 +20,7 @@ make
 
 # create sqlite3 database with keys table
 echo "Creating Observer database..."
-cd ../../db
+cd ../db
 db=observer.db
 
 if [ -f "$db" ]
@@ -29,25 +29,31 @@ then
 else
     sqlite3 $db < configure.sql
 fi
+# make db writeable
+sudo chmod 664 $db
 
 cd ../src/init_download
 pip3 install cython
 echo "Compiling Cython.."
 python3 setup.py build_ext --inplace
 # if you only care about UTxO, then you can skip this (eventually!)
-echo "Do you want to download all addresses that have been used in the last ~584,000 blocks? (y/n):"
-read response
-if [ $response = "y" ]
-then
-    mkdir address_sets
-    echo "Downloading addresses from S3, this will take some time."
-    sh s3_download.sh
-    # load the addresses into the database
-    echo "Done! Loading addresses into sqlite3, this will take a while so it will be run in the background."
-    echo "If at any point you want to quit, just find the process and kill it via"
-    echo "kill -KILL <pid>"
-    echo "For example, ps aux | grep load.sh"
-    echo "Then ps aux | grep load_adresses.py"
-    sh load.sh &
-else
-    echo "Skipping download. You can do this at a later time, just read over the configure.sh script."
+read -r -p "Do you want to download all addresses that have been used in the last ~584,000 blocks? (y/n):" response
+case "$response" in
+    [yY][eE][sS]|[yY]) 
+        mkdir address_sets
+        echo "Downloading addresses from S3, this will take some time."
+        sh s3_download.sh
+        # load the addresses into the database
+        echo "Done! Loading addresses into sqlite3, this will take a while so it will be run in the background."
+        echo "If at any point you want to quit, just find the process and kill it via"
+        echo "kill -KILL <pid>"
+        echo "For example, ps aux | grep load.sh"
+        echo "Then ps aux | grep load_adresses.py"
+        sh load.sh &do_something
+        ;;
+    *)
+        echo "Skipping download. You can do this at a later time, just read over the configure.sh script."
+        ;;
+esac
+
+echo "Done."
