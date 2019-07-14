bfbd22795c486d9c4f47636688764c84aca4d7b6
MellowYarker <milanmiladinovic@outlook.com>
2019-07-14 19:19:26 -0400

    SCAN-8 Massively changed design of S3 download.

--- src/init_download/load.sh
+++ src/init_download/load.sh
@@ -1,3 +1,12 @@
-for i in address_sets/*; do
-    python3 load_addresses.py -f $i
-done
+# Note, used.sql is not in a folder, we download it straight from S3
+$db="observer.db"
+
+echo "Inserting used addresses into database. This will take a while."
+
+sudo sqlite3 ../../db/$db < used.sql
+echo "Done! Inserting all elements into bloom filter..."
+
+sudo make load_filter
+sudo ./load_filter
+echo "Done! You can now compare your generated addresses to over 530 million
+      used addresses!"
